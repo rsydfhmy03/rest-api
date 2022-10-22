e1cd377bedd6af4895c0f58fbddb84981777cf27
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-23 00:49:50 +0700

    commit not fixed

--- JSON/script.js
+++ JSON/script.js
@@ -1,8 +1,18 @@
-let mhs = {
-    nama: "fahmy rosyadi",
-    nim: "e41212254",
-    email: "fahmyrosyadi29@gmail.com"
+// let mhs = {
+//     nama: "fahmy rosyadi",
+//     nim: "e41212254",
+//     email: "fahmyrosyadi29@gmail.com"
 
+// }
+
+// console.log(JSON.stringify(mhs));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        let mhs = this.responseText;
+        console.log(mhs);
+    }
 }
-
-console.log(JSON.stringify(mhs));
+xhr.open('GET','coba.json' , true);
+xhr.send();

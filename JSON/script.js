27d58d3ebbc4f4215220cca5aafd3ee2be795250
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-23 01:01:04 +0700

    commit not fixed

--- JSON/script.js
+++ JSON/script.js
@@ -1,3 +1,4 @@
+//Vanila js
 // let mhs = {
 //     nama: "fahmy rosyadi",
 //     nim: "e41212254",
@@ -7,13 +8,19 @@
 // stringify -> dari object to json
 // console.log(JSON.stringify(mhs));
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        //parse -> dari json to object
-        let mhs = JSON.parse(this.responseText);
-        console.log(mhs);
-    }
-}
-xhr.open('GET','coba.json' , true);
-xhr.send();
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         //parse -> dari json to object
+//         let mhs = JSON.parse(this.responseText);
+//         console.log(mhs);
+//     }
+// }
+// xhr.open('GET','coba.json' , true);
+// xhr.send();
+
+// jquery
+$.getJSON('coba.json', function(data){
+
+    console.log(data);
+})

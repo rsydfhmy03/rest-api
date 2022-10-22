b61a4d548fdf2b880cd6ff285656d6ca6485cedd
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-22 23:55:44 +0700

    commit 3

---
+++ JSON/latihan1.php
@@ -0,0 +1,12 @@
+<?php
+
+$mahasiswa = [
+    "nama" => "Fahmy rosyadi",
+    "nim" => "e41212254",
+    "email" => "fahmyrosyadi29@gmail.com"
+];
+
+//ini pasti bentuknya array
+var_dump($mahasiswa);
+
+?>

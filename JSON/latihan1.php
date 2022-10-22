0400478ad46cee2b26347dbd63ada061252dc425
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-23 00:05:04 +0700

    commit not fixed

--- JSON/latihan1.php
+++ JSON/latihan1.php
@@ -1,9 +1,16 @@
 <?php
 
 $mahasiswa = [
+    [
     "nama" => "Fahmy rosyadi",
     "nim" => "e41212254",
     "email" => "fahmyrosyadi29@gmail.com"
+    ],
+    [
+    "nama" => "rsyd fhmy",
+    "nim" => "e41219913",
+    "email" => "rsydfhmy@gmail.com"
+    ]
 ];
 
 $data = json_encode($mahasiswa);

5b9a397d5d84ecdb25ee00bcf03e21ad511f702f
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-23 00:53:15 +0700

    commit not fixed

--- JSON/script.js
+++ JSON/script.js
@@ -4,13 +4,14 @@
 //     email: "fahmyrosyadi29@gmail.com"
 
 // }
-
+// stringify -> dari object to json
 // console.log(JSON.stringify(mhs));
 
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        let mhs = this.responseText;
+        //parse -> dari json to object
+        let mhs = JSON.parse(this.responseText);
         console.log(mhs);
     }
 }

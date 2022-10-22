8c93c219801138c4f111bfb862afdf5091fdfce7
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-23 00:29:06 +0700

    commit not fixed

--- JSON/latihan1.php
+++ JSON/latihan1.php
@@ -1,17 +1,23 @@
 <?php
 
-$mahasiswa = [
-    [
-    "nama" => "Fahmy rosyadi",
-    "nim" => "e41212254",
-    "email" => "fahmyrosyadi29@gmail.com"
-    ],
-    [
-    "nama" => "rsyd fhmy",
-    "nim" => "e41219913",
-    "email" => "rsydfhmy@gmail.com"
-    ]
-];
+// $mahasiswa = [
+//     [
+//     "nama" => "Fahmy rosyadi",
+//     "nim" => "e41212254",
+//     "email" => "fahmyrosyadi29@gmail.com"
+//     ],
+//     [
+//     "nama" => "rsyd fhmy",
+//     "nim" => "e41219913",
+//     "email" => "rsydfhmy@gmail.com"
+//     ]
+// ];
+$dbh = new PDO('mysql:host=localhost;dbname=mahasiswa', 'root' ,'');
+
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+//Ambil semua datanya lalu ubah kembalian jadi array assoc
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $data = json_encode($mahasiswa);
 echo $data
